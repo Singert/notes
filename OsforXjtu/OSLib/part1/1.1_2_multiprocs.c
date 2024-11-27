6bc2eb22e9067673b201f8e1ef1521e49538ef9c
Singert <551432395a@gmail.com>
2024-11-27 16:38:38 +0800

    fix part1

--- OsforXjtu/OSLib/part1/1.1_2_multiprocs.c
+++ OsforXjtu/OSLib/part1/1.1_2_multiprocs.c
@@ -1,48 +1,48 @@
-# include<stdio.h>
-# include<unistd.h>
-# include<stdlib.h>
-# include<sys/types.h>
-# include<sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int num = -1;
 
-
-int 
-main() o{
-    pid_t rc =fork();
-    if (rc < 0)
+int
+main ()
+{
+  pid_t rc = fork ();
+  if (rc < 0)
     {
-        fprintf(stderr,"fork fail");
-        exit(1);
+      fprintf (stderr, "fork fail");
+      exit (1);
     }
 
-    else if (rc == 0)
+  else if (rc == 0)
     {
-        printf("rc = %d \n",rc);
-        printf("child (pid = %d) begin & num = %d\n",getpid(),num);
-        num = 1;
-        printf("child end & num = %d \n",num);
-        system("./system_call");
-        char *args[3];
-        args[0] = "wc";
-        args[1] = "Makefile";
-        args[2] = NULL;
-        execvp(args[0],args);
-        system("./system_call");
-    }
-    
-    else if (rc > 0)
-    {
-        wait(NULL);
-        printf("rc = %d \n",rc);
-        printf("parent (pid = %d) begin & num  = %d\n",getpid(),num);
-        num = 0;
-        printf("parent end & num = %d\n",num);
+      printf ("rc = %d \n", rc);
+      printf ("child (pid = %d) begin & num = %d\n", getpid (), num);
+      num = 1;
+      printf ("child end & num = %d \n", num);
+      system ("./system_call");
+      char *args[3];
+      args[0] = "wc";
+      args[1] = "Makefile";
+      args[2] = NULL;
+      execvp (args[0], args);
+      system ("./system_call");
     }
 
-    printf("current num = %d\n",num);
-    num = -2;
-    printf("process end & num = %d\n",num);
+  else if (rc > 0)
+    {
+      wait (NULL);
+      printf ("rc = %d \n", rc);
+      printf ("parent (pid = %d) begin & num  = %d\n", getpid (), num);
+      num = 0;
+      printf ("parent end & num = %d\n", num);
+    }
 
-    return 0;
-}   
+  printf ("current num = %d\n", num);
+  num = -2;
+  printf ("process end & num = %d\n", num);
+
+  return 0;
+}
